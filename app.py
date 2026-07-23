32417182aa7b2925a6e8952e2f91eb92f72b3804
PK-06 <pierrick.samson369@gmail.com>
2026-07-23 17:55:42 +0200

    port change 5000 to 10001

--- app.py
+++ app.py
@@ -410,4 +410,4 @@ def create_app() -> Flask:
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=True)
+    app.run(host="0.0.0.0", port=10001, debug=True, use_reloader=True)
--- wsgi.py
+++ wsgi.py
@@ -7,4 +7,4 @@ if app.secret_key == "dev-secret-change-me":
     raise RuntimeError('Please change secret for securtiy')
 
 if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=5000, threads=4)
+    serve(app, host='0.0.0.0', port=10001, threads=4)
