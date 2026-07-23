abf14436d436e1f78eafee3292b1b1833e195c9d
PK-06 <pierrick.samson369@gmail.com>
2026-07-23 12:32:59 +0200

    security add-on

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
 from waitress import serve
 from app import app
+from src.config import Config
 
 app.debug = False
+
+if Config.SECRET_KEY == "dev-secret-change-me":
+    raise RuntimeError('Please change secret for securtiy')
 
 if __name__ == "__main__":
     serve(app, host='0.0.0.0', port=5000, threads=4)
